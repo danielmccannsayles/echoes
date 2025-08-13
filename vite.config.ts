ef31e40d371a091170f26ef99883303e12e0f8e0
Daniel MS <danielmccannsayles@gmail.com>
2025-08-12 17:28:54 -0700

    change vite - pouint to correct audio I hope

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   build: {
     assetsInlineLimit: 0, // Don't inline assets
     rollupOptions: {
       output: {
-        // Use relative imports
-        format: 'es'
-      }
-    }
+        format: "es",
+      },
+    },
   },
-  base: './' // Use relative paths
-})
+  base: "/echoes/", // GH page subdir
+});
