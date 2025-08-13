4ada798085e304cc5bd61b0d7de875a505716658
Daniel MS <danielmccannsayles@gmail.com>
2025-08-12 17:13:26 -0700

    try again

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    assetsInlineLimit: 0, // Don't inline assets
+    rollupOptions: {
+      output: {
+        // Use relative imports
+        format: 'es'
+      }
+    }
+  },
+  base: './' // Use relative paths
 })
